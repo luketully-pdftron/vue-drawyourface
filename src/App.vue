1c87b450fa7ca1d3853e1c509f72d8d445f83c58
Luke <luke@PDFTrons-MBP.pdftron.local>
2019-10-30 10:36:23 -0700

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,89 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <UserList :users="faceData" :selected="currentUser" @update:selected="changeCurrentUser"></UserList>
+    <WebViewer
+      v-on:loaded="handleLoaded"
+      v-on:change="handleChanged"
+      v-bind:publicPath="publicPath"
+      :content="content"
+    ></WebViewer>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WebViewer from "./components/WebViewer.vue";
+import UserList from "./components/UserList.vue";
+import { saveFace, getFace } from "./faceStore.js";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    WebViewer,
+    UserList
+  },
+  computed: {
+    content() {
+      return this.faceData[this.currentUser].content;
+    }
+  },
+  data() {
+    return {
+      publicPath: process.env.BASE_URL,
+      currentUser: 2,
+      faceData: {
+        0: {
+          name: "Rosi",
+          content: null
+        },
+        1: {
+          name: "Tim",
+          content: null
+        },
+        2: {
+          name: "Sarah",
+          content: null
+        },
+        3: {
+          name: "Bob",
+          content: null
+        },
+        4: {
+          name: "Greg",
+          content: null
+        }
+      }
+    };
+  },
+  methods: {
+    handleLoaded(e) {
+      getFace(this.currentUser.toString()).then(value => {
+        this.faceData[this.currentUser].content = value.drawingData || null;
+      });
+    },
+    handleChanged(e) {
+      const newContent = e;
+      const keyAsAstring = this.currentUser.toString();
+      this.faceData[this.currentUser].content = e;
+      saveFace(keyAsAstring, this.faceData[keyAsAstring].content);
+    },
+    changeCurrentUser(e) {
+      this.currentUser = e;
+      getFace(this.currentUser.toString()).then(value => {
+        this.faceData[this.currentUser].content = value.drawingData || null;
+      });
+    }
+  },
+  mounted: function() {
+    // Init currentUser
+    this.currentUser = 2;
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
