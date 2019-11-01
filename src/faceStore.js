d7749c3eed95bd44b96925777ce69320be73cdc8
Luke <luke@PDFTrons-MacBook-Pro.local>
2019-11-01 14:51:57 -0700

    Fix another typo

--- src/faceStore.js
+++ src/faceStore.js
@@ -7,6 +7,6 @@ export const getFace = (userId) => {
 
 export const saveFace = (userId, face) => {
     /* Save the current annotation data along with it's timestamp */
-    const currentTime = new Date().now(); // Store the UTC timestamp
+    const currentTime = Date.now(); // Store the UTC timestamp
     localforage.setItem(userId, {drawingData: face, timestamp: currentTime.toString()});
 }
