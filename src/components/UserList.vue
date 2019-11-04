96aa35b0ed2561f0daa9bae5530a53c38bc4352c
Luke <luke@PDFTrons-MBP.pdftron.local>
2019-11-04 12:28:39 -0800

    Swap native select for Element-ui Select comp

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,24 +1,42 @@
 <template>
   <form action="#">
     <h2>{{title}}</h2>
-    <select name="userSelect" id="userSelector" @change="handleUserChange" :value="selected">
-      <option v-for="(user,id) in users" :key="id" v-bind:value="id">{{ user.name }}</option>
-    </select>
+    <el-select
+      name="userSelect"
+      id="userSelector"
+      @change="handleUserChange"
+      :value="selected"
+      placeholder="Select"
+    >
+      <el-option v-for="(user,id) in users" :label="user.name" :key="id" :value="id">{{ user.name }}</el-option>
+    </el-select>
     <p>Current user: {{users[selected].name}} who is {{selected}}</p>
   </form>
 </template>
 
 <script>
+import { Select, Option } from "element-ui";
+import Vue from "vue";
+
+
+/* Register Element-ui components */
+Vue.component(Select.name, Select);
+Vue.component(Option.name, Option);
+
 export default {
   data() {
     return {
       title: "User Selection List"
     };
   },
+  components: {
+    Select,
+    Option
+  },
   props: ["selected", "users"],
   methods: {
-    handleUserChange(e) {
-      this.$emit("update:selected", parseInt(e.currentTarget.value, 10));
+    handleUserChange(userId) {
+      this.$emit("update:selected", parseInt(userId, 10));
     }
   }
 };
